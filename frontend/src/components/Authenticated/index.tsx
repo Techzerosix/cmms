f333d2ef737703f6fd959910c892d07441f8a095
Techzerosix <danijel.kedves@gmail.com>
2026-01-06 17:30:40 +0100

    Update index.tsx

--- frontend/src/components/Authenticated/index.tsx
+++ frontend/src/components/Authenticated/index.tsx
@@ -1,8 +1,7 @@
-import { FC, ReactNode, useState } from 'react';
+import { FC, ReactNode } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import useAuth from 'src/hooks/useAuth';
-import Login from 'src/content/pages/Auth/Login/Cover';
 
 interface AuthenticatedProps {
   children: ReactNode;
@@ -12,20 +11,15 @@ const Authenticated: FC<AuthenticatedProps> = (props: { children: any }) => {
   const { children } = props;
   const auth = useAuth();
   const location = useLocation();
-  const [requestedLocation, setRequestedLocation] = useState<string | null>(
-    null
-  );
 
   if (!auth.isAuthenticated) {
-    if (location.pathname !== requestedLocation) {
-      setRequestedLocation(location.pathname);
-    }
-    return <Login />;
-  }
-
-  if (requestedLocation && location.pathname !== requestedLocation) {
-    setRequestedLocation(null);
-    return <Navigate to={requestedLocation} />;
+    return (
+      <Navigate
+        to="/account/login"
+        replace
+        state={{ from: location }}
+      />
+    );
   }
 
   return <>{children}</>;
